d4bb648a5da5ee38c21e995ac1c47686c73a12d1
nabeff <neffina8@gmail.com>
2023-12-19 22:48:02 +0100

    footer picture

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -8,8 +8,8 @@ const Footer = () => {
     <div className={styles.container}>
       <div className={styles.info}>
         <div className={styles.logo}>
-          <Image src="/logo.png" alt="lama blog" width={50} height={50} />
-          <h1 className={styles.logoText}>NabilBlogg</h1>
+          <Image src="/nabil-effina-BW.jpeg" alt="nabil effina" width={50} height={50} className={styles.radius}/>
+          <h1 className={styles.logoText}>Effina Nabil</h1>
         </div>
         <p className={styles.desc}>
         Give that arrow a tap on any story to read or comment. And once you're in, get ready to write and sprinkle your thoughts like confetti. Let's join the fun!
